eb9939efe55c1272dd8d743ad9b639a631622ee3
kunals12 <work.kunal12@gmail.com>
2024-12-03 20:11:07 +0530

    tests: create_entry

--- anchor/tests/crudapp.spec.ts
+++ anchor/tests/crudapp.spec.ts
@@ -1,17 +1,34 @@
-import * as anchor from '@coral-xyz/anchor'
 import {Program} from '@coral-xyz/anchor'
-import {Keypair} from '@solana/web3.js'
+import {Keypair, PublicKey} from '@solana/web3.js'
 import {Crudapp} from '../target/types/crudapp'
+import { BankrunProvider, startAnchor } from 'anchor-bankrun'
+
+const IDL = require("../target/idl/crudapp.json");
+const crudAppAddress = new PublicKey("D9fofwkjqYzDUUHKudbQ2UVxRDji7gwFx8aJJx1bdkbp");
 
 describe('crudapp', () => {
-  // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env()
-  anchor.setProvider(provider)
-  const payer = provider.wallet as anchor.Wallet
+  let context;
+  let provider: BankrunProvider;
+  let crudAppProgram: Program<Crudapp>;
 
-  const program = anchor.workspace.Crudapp as Program<Crudapp>
-
-  const crudappKeypair = Keypair.generate()
-
+  beforeAll(async() => {
+    context = await startAnchor("", [{name: "crudapp", programId: crudAppAddress}], []);
+    provider = new BankrunProvider(context);
+    crudAppProgram = new Program<Crudapp>(IDL, provider);
+  })
   
+  it("create entry", async() => {
+    const title = "Hello";
+    await crudAppProgram.methods.createJournalEntry(
+      title,
+      "Hello World"
+    ).rpc();
+    
+    const [entryAddress] = PublicKey.findProgramAddressSync([Buffer.from(title), provider.wallet.publicKey.toBuffer()], crudAppAddress);
+    // console.log({entryAddress});
+    
+    const entry = await crudAppProgram.account.journalEntryState.fetch(entryAddress)
+    // console.log(entry);
+    expect(entry.title).toEqual(title);
+  });
 })
